6330205de62f7f1572835695ef5d5e3517f9fd0a
rahulcb03 <rahulcb03@gmail.com>
2024-02-24 12:07:26 -0500

    First Commit

---
+++ src/main/java/com/rahul/WordGames/WordGamesApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.WordGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordGamesApplication.class, args);
+	}
+
+}
